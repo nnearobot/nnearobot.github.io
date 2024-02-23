61452671b88de3c851ae4b25b7d76a6cd7565a7e
Rimma Maksiutova <newtonnord@gmail.com>
2024-02-23 19:55:19 +0900

    footer and container styles fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {   
     return (
-        <footer className="py-6 px-4 max-w-screen-xl mx-auto md:px-8">
+        <footer className="mt-10 py-6 px-4 max-w-screen-xl mx-auto md:px-8">
             <div className='text-right'>
                 &copy; 2023&mdash;2024 Rimma Maksiutova
             </div>
--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -8,7 +8,7 @@ const Container = ({...props}) => {
     className = className || ""
 
     return (
-        <div className={`container max-w-4xl mx-auto p-2 xs:p-3 sm:p-6 md:p-10 ${className}`}>
+        <div className={`container max-w-4xl mx-auto px-2 xs:px-3 sm:px-6 md:px-10 ${className}`}>
             {props.children}
         </div>
     );
