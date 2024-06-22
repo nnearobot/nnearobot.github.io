a111c7a518cc22d490c55e7619af651d6ee8d131
Rimma Maksiutova <newtonnord@gmail.com>
2024-06-22 21:20:20 +0900

    Button styles

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,6 +1,22 @@
+import { Exo_2 } from 'next/font/google';
+
+const exo2 = Exo_2({
+    weight: ['400'],
+    subsets: ['latin']
+})
+
 const Button = ({...props}) => {
+
+    let {
+        children,
+        className,
+        ...rest
+    } = props;
+
+    className = className || "";
+
     return (
-        <button {...props} className='bg-zinc-400 color-white'>
+        <button {...props} className={`px-5 py-1 border rounded-md font-bold md:text-lg capitalize ${exo2.className} bg-zinc-700 text-white active:bg-zinc-900 ${className}`}>
             {props.children}
         </button>
     );
