a3209451327b77d5023a08e1e267ce99ce046460
Rimma Maksiutova <newtonnord@gmail.com>
2026-02-23 23:02:46 +0900

    logo tri -> @

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,24 +1,13 @@
 import styles from "./Logo.module.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+
+
 const Logo = () => {
     return (
         <div className={styles.logo} aria-label="nnearobot logo">
-            <svg className={styles.tri} xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 800">
-                <defs>
-                    <filter id="neon-filter" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                        <feGaussianBlur stdDeviation="34 34" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur>
-                    </filter>
-                    <filter id="neon-filter2" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                        <feGaussianBlur stdDeviation="18 18" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur>
-                    </filter>
-                </defs>
-                <g strokeWidth="34.5" stroke="" fill="none" transform="rotate(180, 400, 400)">
-                    <polygon points="400,92.5 92.5,625.1 707.5,625.1" filter="url(#neon-filter)"></polygon>
-                    <polygon points="410,92.5 102.5,625.1 717.5,625.1" filter="url(#neon-filter2)" opacity="0.14"></polygon>
-                    <polygon points="390,92.5 82.5,625.1 697.5,625.1" filter="url(#neon-filter2)" opacity="0.14"></polygon>
-                    <polygon points="400,92.5 92.5,625.1 707.5,625.1"></polygon>
-                </g>
-            </svg>
+            <FontAwesomeIcon icon={faAt} className={styles.icon} aria-hidden="true" />
             <span className={styles.wordmark}>nnearobot</span>
         </div>
     );
